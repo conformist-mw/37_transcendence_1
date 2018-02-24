c321609f791c6286284547c1518db16a52fd5cc5
conformist-mw <conformist.mw@gmail.com>
2018-02-24 19:19:02 +0200

    fix path, slashes

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -13,8 +13,8 @@ class BaseConfig(Configuration):
     USE_I18N = True
     USE_L10N = True
     USE_TZ = True
-    STATIC_URL = 'static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     WSGI_APPLICATION = 'transcendence.wsgi.application'
 
     INSTALLED_APPS = [
