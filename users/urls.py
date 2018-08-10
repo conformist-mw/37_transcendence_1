1566392f713dd3fccc2afc3febd40c1a33beaec2
Oleg Smedyuk <conformist.mw@gmail.com>
2018-08-10 11:02:00 +0300

    added access to users by username instead of pk

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from users.views import UserDetail
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('<int:pk>/', UserDetail.as_view()),
+    path('<str:username>/', UserDetail.as_view()),
+    path('login/', auth_views.login, name='login'),
+    path('logout', auth_views.logout, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,23 @@
-from django.contrib.auth.models import User
 from django.views.generic import DetailView
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+from .models import User
 
 
 class UserDetail(DetailView):
     model = User
     context_object_name = 'user'
     template_name = 'users/user_detail.html'
+
+    def get_object(self):
+        if self.kwargs.get('pk'):
+            return get_object_or_404(User, pk=self.kwargs['pk'])
+        return get_object_or_404(User, username=self.kwargs['username'])
+
+    def get(self, *args, **kwargs):
+        self.object = self.get_object()
+        if kwargs.get('pk'):
+            return HttpResponseRedirect(self.object.user_url)
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
